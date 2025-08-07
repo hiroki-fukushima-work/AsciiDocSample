63e56279224882e2b61069dd66aef96d51a38694
hrfukusi <hrfukusi@fsi.co.jp>
2025-08-07 11:03:30 +0900

    initialScriptsの扱いを修正

--- initialScripts/startKroki.bat
+++ initialScripts/startKroki.bat
@@ -12,6 +12,6 @@ echo %~d0%~p0
 
 REM PlantUMLのJARファイルのパスを指定
 set KROKI_LISTEN=0.0.0.0:60006
-java -jar ..\lib\kroki-standalone-server-v0.26.0.jar
+java -jar .\lib\kroki-standalone-server-v0.26.0.jar
 
 endlocal
